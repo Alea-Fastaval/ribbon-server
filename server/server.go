63d6af7fb49fb39dff723d6ebb9405bee5d3257a
DreamSpawn <westh_mikkel@hotmail.com>
2024-04-26 18:40:04 +0200

    ADDED information about needing root if permission denied on creating socket

--- server/server.go
+++ server/server.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/user"
 	"strconv"
+	"strings"
 )
 
 type Server struct {
@@ -16,6 +17,10 @@ type Server struct {
 func (server *Server) Start(socket_path string) {
 	socket, err := net.Listen("unix", socket_path)
 	if err != nil {
+		fmt.Printf("\nCouldn't bind socket %s\n", socket_path)
+		if strings.Contains(err.Error(), "permission denied") {
+			fmt.Print("\x1b[41mYou may need to run the server as root\x1b[0m\n\n")
+		}
 		panic(err)
 	}
 	server.socket = socket
