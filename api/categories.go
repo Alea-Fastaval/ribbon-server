44859f1d1a6126f4f972263fe63622b321cef511
DreamSpawn <westh_mikkel@hotmail.com>
2024-05-13 01:45:18 +0200

    ADDED can now create categories via API and database function ADDED categories are now sorted by odering value when returned by the API CHANGED color names in Category

--- api/categories.go
+++ api/categories.go
@@ -3,13 +3,45 @@ package api
 import (
 	"fmt"
 	"net/url"
+	"slices"
+	"strings"
 
 	"github.com/dreamspawn/ribbon-server/database"
 )
 
 func categoriesAPI(sub_path string, vars url.Values, method string) (any, error) {
 	if method == "GET" {
-		return database.GetCategories()
+		categories, err := database.GetCategories()
+		if err != nil {
+			return categories, err
+		}
+		slices.SortFunc(categories, func(a, b database.Category) int {
+			return int(a.Ordering) - int(b.Ordering)
+		})
+
+		return categories, nil
+	}
+
+	if method == "POST" {
+		new_category, err := database.CreateCategory(
+			vars["background_color"][0],
+			vars["stripes_color"][0],
+			vars["glyph_color"][0],
+		)
+
+		if err != nil {
+			fmt.Printf("Failed to create new category with values %+v\n", vars)
+			panic(err)
+		}
+
+		for key, value := range vars {
+			lang, found := strings.CutPrefix(key, "name_")
+			if found {
+				database.AddTranslation(lang, "categories."+fmt.Sprint(new_category.ID), value[0])
+			}
+		}
+
+		return new_category, err
 	}
 
 	return nil, fmt.Errorf("endpoint not implemented for method %s", method)
--- database/category.go
+++ database/category.go
@@ -1,10 +1,11 @@
 package database
 
 type Category struct {
-	id         uint
-	foreground string
-	background string
-	ordering   uint
+	ID         uint
+	Background string
+	Stripes    string
+	Glyph      string
+	Ordering   uint
 }
 
 func GetCategories() ([]Category, error) {
@@ -18,7 +19,7 @@ func GetCategories() ([]Category, error) {
 	for rows.Next() {
 		category := Category{}
 
-		err := rows.Scan(&category.id, &category.foreground, &category.background, &category.ordering)
+		err := rows.Scan(&category.ID, &category.Background, &category.Stripes, &category.Glyph, &category.Ordering)
 		if err != nil {
 			return nil, db_error(statement, nil, err)
 		}
@@ -27,4 +28,31 @@ func GetCategories() ([]Category, error) {
 	}
 
 	return result, nil
+}
+
+func CreateCategory(background, stripes, glyph string) (*Category, error) {
+	var category_count int
+	statement := "SELECT COUNT(*) FROM categories"
+	row := db.QueryRow(statement)
+	err := row.Scan(&category_count)
+	if err != nil {
+		return nil, db_error(statement, nil, err)
+	}
+
+	statement = "INSERT INTO categories(background, stripes, glyph, ordering) VALUES(?,?,?,?)"
+	result, err := db.Exec(statement, background, stripes, glyph, category_count)
+	if err != nil {
+		return nil, db_error(statement, nil, err)
+	}
+
+	id, err := result.LastInsertId()
+
+	new_category := Category{
+		uint(id),
+		background,
+		stripes,
+		glyph,
+		uint(category_count),
+	}
+	return &new_category, err
 }
