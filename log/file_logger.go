d2d33caba929e849f138e3d95f41a0c5f03ed385
DreamSpawn <westh_mikkel@hotmail.com>
2024-06-11 22:59:12 +0200

    CHANGED permissions for log file

--- log/file_logger.go
+++ log/file_logger.go
@@ -10,7 +10,7 @@ import (
 
 func Init() {
 	logfile_path := config.Get("resource_dir") + "log.txt"
-	logfile, err := os.OpenFile(logfile_path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	logfile, err := os.OpenFile(logfile_path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		fmt.Printf("Could not open log file: %s\n", logfile_path)
 		return
